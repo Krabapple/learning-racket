86d5c41b40f827a7570ea29de257d4312c5d0d84
Krabapple <krabapple123@gmail.com>
2016-12-05 23:26:29 -0500

    Create lambda recursion.rkt
    How to do recursion without using explicit recursion or even without calling the function's name.

---
+++ lambda recursion.rkt
@@ -0,0 +1,15 @@
+;; A function that consumes a natural number and produces its factorial
+;;   using lambda recursion.
+
+((lambda (x) (x x))
+ (lambda (fact-gen)
+   (lambda (n)
+     (if (zero? n)
+         1
+         (* n ((fact-gen fact-gen) (sub1 n)))))))
+        
+;; Pretty fun.
+;; For more details, check the following pages:
+;; http://stackoverflow.com/questions/7719004/in-scheme-how-do-you-use-lambda-to-create-a-recursive-function
+;; http://zzp.me/2013-09-05/y-combinator/ (Fous on the deduplication part.)
+;; https://trow.cc/board/showtopic=16485
